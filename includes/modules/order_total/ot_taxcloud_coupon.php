ecc9880cc0ff4427280585a64fc81f474d40e497
Scott C Wilson <scottcwilson@gmail.com>
2015-03-06 20:15:38 -0500

    Zen Cart 1.5.4 Updates Merges required for TaxCloud to run with Zen Cart 1.5.4.

--- includes/modules/order_total/ot_taxcloud_coupon.php
+++ includes/modules/order_total/ot_taxcloud_coupon.php
@@ -49,51 +49,51 @@ class ot_taxcloud_coupon extends ot_coupon {
  * The main difference between this and the original method (in ot_coupon) is that we don't discount the tax.
  *
  */
-function calculate_deductions($order_total) {
-	global $db, $order, $messageStack, $currencies;
-      	$tax_address = zen_get_tax_locations();
-      	$od_amount = array();
-      	$orderTotalDetails = $this->get_order_total();
-      	$orderTotalTax = $orderTotalDetails['tax'];
-      	$orderTotal = $orderTotalDetails['total'];
-      	if ($_SESSION['cc_id']) {
-        	$coupon = $db->Execute("select * from " . TABLE_COUPONS . " where coupon_id = '" . (int)$_SESSION['cc_id'] . "'");
-        	$this->coupon_code = $coupon->fields['coupon_code'];
-        	
-        	if (($coupon->RecordCount() > 0 && $orderTotal !=0) || ($coupon->RecordCount() > 0 && $coupon->fields['coupon_type']=='S') ) {
-
-          		if (strval($orderTotalDetails['total']) >= $coupon->fields['coupon_minimum_order']) {
+  function calculate_deductions() {
+    global $db, $order, $messageStack, $currencies;
+    $tax_address = zen_get_tax_locations();
+    $od_amount = array();
+    $orderTotalDetails = $this->get_order_total($_SESSION['cc_id']);
+    $orderTotalTax = $orderTotalDetails['tax'];
+    $orderTotal = $orderTotalDetails['total'];
+    if ($_SESSION['cc_id']) {
+      $coupon = $db->Execute("select * from " . TABLE_COUPONS . " where coupon_id = '" . (int)$_SESSION['cc_id'] . "'");
+      $this->coupon_code = $coupon->fields['coupon_code'];
+      
+      if (($coupon->RecordCount() > 0 && $orderTotal !=0) || ($coupon->RecordCount() > 0 && $coupon->fields['coupon_type']=='S') ) {
   
-            			if ($coupon->fields['coupon_type']=='S') {  
-              				$od_amount['total'] = $_SESSION['shipping']['cost'];
-              				$od_amount['type'] = 'S';
-              				$od_amount['tax'] = ($this->calculate_tax == 'Standard') ? $_SESSION['shipping_tax_amount'] : 0;
-              				if (DISPLAY_PRICE_WITH_TAX == 'true')
-              				{
-                				$od_amount['total'] += $od_amount['tax'];
-              				}
-              				if (isset($_SESSION['shipping_tax_description']) && $_SESSION['shipping_tax_description'] != '') {
-                				$od_amount['tax_groups'][$_SESSION['shipping_tax_description']] = $od_amount['tax'];
-              				}
-              				return $od_amount;
-            			}
-            			if ($coupon->fields['coupon_type'] == 'P') {  
-              				$od_amount['total'] = round($orderTotal*($coupon->fields['coupon_amount']/100), 2);
-              				$od_amount['type'] = 'P';
-            			} elseif ($coupon->fields['coupon_type'] == 'F') {
-              				$od_amount['total'] = round($coupon->fields['coupon_amount'] * ($orderTotal>0), 2);
-              				$od_amount['type'] = 'F';
-            			}
-            			if ($od_amount['total'] > $orderTotal) $od_amount['total'] = $orderTotal;
-
-          		} else {
-            			$messageStack->add_session('redemptions', sprintf(TEXT_INVALID_REDEEM_COUPON_MINIMUM, $currencies->format($coupon->fields['coupon_minimum_order'])),'caution');
-            			$this->clear_posts();
-            			zen_redirect(zen_href_link(FILENAME_CHECKOUT_PAYMENT, '', 'SSL',true, false));
-          		}
-          	}
-      	}
-      return $od_amount;
+          if (strval($orderTotalDetails['total']) >= $coupon->fields['coupon_minimum_order']) {
+    
+              if ($coupon->fields['coupon_type']=='S') {  
+                  $od_amount['total'] = $_SESSION['shipping']['cost'];
+                  $od_amount['type'] = 'S';
+                  $od_amount['tax'] = ($this->calculate_tax == 'Standard') ? $_SESSION['shipping_tax_amount'] : 0;
+                  if (DISPLAY_PRICE_WITH_TAX == 'true')
+                  {
+                    $od_amount['total'] += $od_amount['tax'];
+                  }
+                  if (isset($_SESSION['shipping_tax_description']) && $_SESSION['shipping_tax_description'] != '') {
+                    $od_amount['tax_groups'][$_SESSION['shipping_tax_description']] = $od_amount['tax'];
+                  }
+                  return $od_amount;
+              }
+              if ($coupon->fields['coupon_type'] == 'P') {  
+                  $od_amount['total'] = round($orderTotal*($coupon->fields['coupon_amount']/100), 2);
+                  $od_amount['type'] = 'P';
+              } elseif ($coupon->fields['coupon_type'] == 'F') {
+                  $od_amount['total'] = round($coupon->fields['coupon_amount'] * ($orderTotal>0), 2);
+                  $od_amount['type'] = 'F';
+              }
+              if ($od_amount['total'] > $orderTotal) $od_amount['total'] = $orderTotal;
+  
+          } else {
+              $messageStack->add_session('redemptions', sprintf(TEXT_INVALID_REDEEM_COUPON_MINIMUM, $currencies->format($coupon->fields['coupon_minimum_order'])),'caution');
+              $this->clear_posts();
+              zen_redirect(zen_href_link(FILENAME_CHECKOUT_PAYMENT, '', 'SSL',true, false));
+          }
+       }
+    }
+    return $od_amount;
   }
   
   function check() {
